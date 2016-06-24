8e088d96278dc279bc43835be46dd569cbb602e7
orianne11 <orianne.r11@gmail.com>
2016-06-24 16:45:43 +0200

    fini les exercices 1 à 3

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not'."
+puts 'I "said" do not touch this.'
+#puts "j'aime ruby ;)"
+
